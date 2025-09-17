366842ba3f1d3c39af23d2a2bcfaa76c40caa356
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-09-17 11:00:05 -0300

    Adicionando funções e endpoints da API

--- app.js
+++ app.js
@@ -11,9 +11,12 @@
  *****************************************************************************************************************************/
 
 //Import das dependencias da API
-const express       = require('express')     //Responsavel pela API
-const cors          = require('cors')        //Responsavel pelas permissões da API
-const bodyParser    = require('body-parser') //Responsavel por gerenciar a chegada dos dados da API com  o front
+const express = require('express')     //Responsavel pela API
+const cors = require('cors')        //Responsavel pelas permissões da API
+const bodyParser = require('body-parser') //Responsavel por gerenciar a chegada dos dados da API com  o front
+
+//Import do arquivo de funções
+const dados = require('./modulo/funcoes')
 
 // Retorna a porta do servidor atual ou colocamos uma porta local
 const PORT = process.PORT || 8080
@@ -22,7 +25,7 @@ const PORT = process.PORT || 8080
 const app = express()
 
 //Configuração de permissões
-app.use((request, response, next)=>{
+app.use((request, response, next) => {
     response.header('Acess-Control-Allow-Origin', '*')    //Servidor de origem da API
     response.header('Acess-Control-Allow-Methods', 'GET') //Verbos permitidos na API
     // Carrega as configurações no CORS da API
@@ -30,8 +33,73 @@ app.use((request, response, next)=>{
     next() // Próximo, carregar os proximos endPoints
 })
 
+// Request -> chegada de dados na API
+// Response -> retorno de dados na API
+
+
 //ENDPOINTS
 
-app.get('/v1/estados', function(request,response){
+app.get('/v1/estados', function (request, response) {
+    // Pesquisa na função de estados
+    let estados = dados.getAllEstados()
 
+    // Retorna o status code e o JSON
+    response.status(estados.status_code).json(estados)
+})
+
+app.get('/v1/estado/:uf', function (request, response) {
+    // Pedindo uma UF pela requisição
+    let sigla = request.params.uf
+
+    // Pesquisa na função de estado
+    let estado = dados.getEstadoBySigla(sigla)
+
+    // Retorna o status code e o JSON
+    response.status(estado.status_code).json(estado)
+})
+
+app.get('/v1/capital/:uf', function (request, response) {
+    // Pedindo uma UF pela requisição
+    let sigla = request.params.uf
+
+    // Pesquisa na função de buscar capital
+    let capital = dados.getCapitalBySigla(sigla)
+
+    // Retorna o status code e o JSON
+    response.status(capital.status_code).json(capital)
+})
+
+app.get('/v1/estados/regiao/:regiao', function (request, response) {
+    // Pedindo uma região pela requisição
+    let regiao = request.params.regiao
+
+    // Pesquisando os estados de uma região
+    let estados = dados.getEstadosByRegiao(regiao)
+
+    // Retorna o status code e o JSON
+    response.status(estados.status_code).json(estados)
+})
+
+app.get('/v1/capitalPais', function (request, response) {
+    // Pesquisando quais estados já foram capital do país
+    let capitalPais = dados.getVerifyCapitaisDoPais()
+
+    // Retorna o status code e o JSON
+    response.status(capitalPais.status_code).json(capitalPais)
+})
+
+app.get('/v1/cidades/estado/:sigla', function (request, response) {
+    // Pedindo uma UF pela requisição
+    let sigla = request.params.sigla
+
+    // Pesquisa de cidades de um estado
+    let cidades = dados.getCidadesBySigla(sigla)
+
+    // Retorna o status code e o JSON
+    response.status(cidades.status_code).json(cidades)
+})
+
+//Start na API
+app.listen(PORT, function () {
+    console.log('API aguardando requisições ...')
 })
--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -11,18 +11,26 @@ const MESSAGE_ERRO = {
     development: "Gabryel Fillipe Cavalcanti da Silva"
 }
 
+//import do arquivo de estados e cidades
 const dados = require('./estados_cidades.js')
 
 
 // Retorna todos os estados
 const getAllEstados = function () {
 
-    let message = { status: true, status_code: 200, development: "Gabryel Fillipe Cavalcanti da silva", uf: []}
+    let message = {
+        status: true,
+        status_code: 200,
+        development: "Gabryel Fillipe Cavalcanti da silva",
+        uf: []
+    }
 
     dados.listaDeEstados.estados.forEach(function (item) {
         message.uf.push(item.sigla)
     })
+    // Para adicionar um elemento novo no JSON
     message.quantidade = message.uf.length
+
 
     if (message.uf.length > 0)
         return message //Verdadeira 200
@@ -30,41 +38,169 @@ const getAllEstados = function () {
         return MESSAGE_ERRO //Falsa 500
 }
 
-
 // Retorna um estado pesquisando pela sigla
 const getEstadoBySigla = function (sigla) {
-    let uf = sigla.toUpperCase()
-    let estadoDesejado = dados.listaDeEstados.estados.find(estado => estado.sigla === uf)
 
-    if(estadoDesejado != null)
-        return 
-    else
+    //Tranformando a sigla digitada em uma variavel pronta e tratada para letra maiuscula 
+    let uf = sigla.toUpperCase()
+
+    //Variáriavel que verifica a sigla digitada
+    let infoEstado = dados.listaDeEstados.estados.find(estado => estado.sigla === uf)
+
+    // Verificando se o estado digitado retornou um JSON com suas informações
+    if (infoEstado == undefined) {
         return MESSAGE_ERRO
-    console.log(estadoDesejado)
+    } else {
+
+        // Pegando informações do estado desejado e passando elas para variáveis 
+        let nomeEstado = infoEstado.nome
+        let capital = infoEstado.capital
+        let regiao = infoEstado.regiao
+
+        //Criando o JSON e colocando as informações do estado desejado
+        let estadoDesejado = { status: true, status_code: 200, development: "Gabryel Fillipe Cavalcanti da silva", uf: uf, descricao: nomeEstado, capital: capital, regiao: regiao }
+
+        return estadoDesejado
+    }
 }
 
 // Retorna a capital referente a um estado pesquisando pela sigla
 const getCapitalBySigla = function (sigla) {
 
+    //Tranformando a sigla digitada em uma variavel pronta e tratada para letra maiuscula 
+    let uf = sigla.toUpperCase()
+
+    //Variáriavel que verifica a sigla digitada
+    let estadoDigitado = dados.listaDeEstados.estados.find(estado => estado.sigla === uf)
+
+
+    // Verificando se o estado digitado retornou um JSON com suas informações
+    if (estadoDigitado == undefined) {
+        return MESSAGE_ERRO
+    } else {
+
+        // Variáveis para pegar capital e nome do estado digitado
+        let descricao = estadoDigitado.nome
+        let capital = estadoDigitado.capital
+
+        // Variável responsavel por criar o JSON de resposta 
+        let informacoesCapital = {
+            status: true,
+            status_code: 200, development: "Gabryel Fillipe Cavalcanti da silva",
+            uf: uf,
+            descricao: descricao,
+            capital: capital
+        }
+
+        return informacoesCapital
+    }
 }
 
 // Retorna uma lista de estados pesquisando pela região
 const getEstadosByRegiao = function (regiao) {
 
+    let estadosRegiao = {
+        status: true,
+        status_code: 200,
+        development: "Gabryel Fillipe Cavalcanti da silva",
+        regiao: regiao,
+        estados: []
+    }
+    dados.listaDeEstados.estados.forEach(function (item) {
+        if (item.regiao == regiao) {
+            estadosRegiao.estados.push({ uf: item.nome, descricao: item.nome })
+        }
+    })
+    if (estadosRegiao.estados.length > 0)
+        return estadosRegiao
+    else
+        return MESSAGE_ERRO
 }
 
 // Retorna uma lista de estados referentes as capitais do páis
 const getVerifyCapitaisDoPais = function () {
 
+
+
+    let capitalPais = {
+        status: true,
+        status_code: 200,
+        development: "Gabryel Fillipe Cavalcanti da silva",
+        capitais: []
+    }
+    dados.listaDeEstados.estados.forEach(function (item) {
+        if (item.capital_pais) {
+
+            let capitalAtual = item.capital_pais.capital
+            let sigla = item.sigla
+            let nome = item.nome
+            let capitalEstado = item.capital
+            let regiao = item.regiao
+            let anoInicioCapital = item.capital_pais.ano_inicio
+            let anoTerminoCapital = item.capital_pais.ano_fim
+
+
+
+            capitalPais.capitais.push({
+                capital_atual: capitalAtual,
+                uf: sigla,
+                descricao: nome,
+                capital: capitalEstado,
+                regiao: regiao,
+                capital_pais_ano_inicio: anoInicioCapital,
+                capital_pais_ano_termino: anoTerminoCapital
+            })
+
+            
+        }
+        
+    })
+    return capitalPais
 }
 
 // Retora uma lista de cidades referente ao estado pesquisado
 const getCidadesBySigla = function (estado) {
+    let uf = estado.toUpperCase()
+
+    // Variável que verifica a sigla digitada e procura no JSON de estados
+    let findCidades = dados.listaDeEstados.estados.find(estado => estado.sigla === uf)
+
+    // Tratando erro para que se o JSON de cidades não seja encontrado ou undefined exiba a mensagem de erro
+    if (findCidades == undefined) {
+        return MESSAGE_ERRO
+    } else {
+
+        // Criando variáveis para passar descrição e quantidade de cidades da UF escolhida
+        let descricao = findCidades.nome
+        let quantidade = findCidades.cidades.length
+
+        // Criando JSON com dados da UF escolhida, quantidade de cidades e os nomes das cidades
+        let cidadesEstado = {
+            status: true,
+            status_code: 200,
+            development: "Gabryel Fillipe Cavalcanti da silva",
+            uf: uf,
+            descricao: descricao,
+            quantidade_cidades: quantidade,
+            cidades: []
+        }
+
+        // Função para adicionar as cidades com base o estado da região escolhida 
+        findCidades.cidades.forEach(function (item) {
+            cidadesEstado.cidades.push(item.nome)
+        })
+        return cidadesEstado
+    }
 
 }
-getEstadoBySigla('sp')
+getVerifyCapitaisDoPais()
 
 
 module.exports = {
     getAllEstados,
+    getEstadoBySigla,
+    getCapitalBySigla,
+    getEstadosByRegiao,
+    getVerifyCapitaisDoPais,
+    getCidadesBySigla
 }
